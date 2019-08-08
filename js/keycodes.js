d8a5615e27aac48457d52b805fdf9c18d6bcc2ae
blueappdev <appdev@bluewin.ch>
2019-08-08 14:49:20 +0200

    key codes viewer

---
+++ js/keycodes.js
@@ -0,0 +1,48 @@
+"use strict";
+
+console.log('keycodes.js - begin');
+
+var feedback = [];
+
+function pageLoaded () {
+    console.log('pageLoaded() - begin');
+    $("#input").keypress(handler);
+    $("#input").keydown(handler);
+    $("#input").keyup(handler);
+    console.log('pageLoaded() - end');
+}
+
+function handler(event) {
+    addEvent(event);
+}
+
+function addEvent(event) {
+    addFeedback(
+        event.type, 
+        event.key, 
+        event.location,
+        event.which,
+        event.charCode,
+        event.keyCode,
+        event.originalEvent.type, 
+        event.originalEvent.key, 
+        event.originalEvent.location,
+        event.originalEvent.which,
+        event.originalEvent.charCode,
+        event.originalEvent.keyCode);
+}
+
+function addFeedback() {
+    console.log("addFeedback(%o)", arguments);
+    var html = '<tr>';
+    for (var i in arguments) {
+        html += '<td>';
+        html += arguments[i];
+        html += '</td>';
+    }
+    html += '</tr>';
+    $("#feedback-table").find("tbody").prepend(html); 
+}
+
+console.log('keycodes.js - end');
+
